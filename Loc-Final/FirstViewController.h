6eacc285ed609ab4d2bcdc38ead13d3aa1c01dd7
kidslovely <lzz198384@gmail.com>
2018-06-23 23:45:01 +0800

    update

--- Loc-Final/FirstViewController.h
+++ Loc-Final/FirstViewController.h
@@ -10,6 +10,7 @@
 #import <MapKit/MapKit.h>
 #import <CoreLocation/CoreLocation.h>
 #import "databaseObject.h"
+#import "SettingViewController.h"
 
 @interface FirstViewController : UIViewController
 
@@ -17,4 +18,5 @@
 @property (weak, nonatomic) IBOutlet MKMapView *mapView;
 @property (strong, nonatomic) databaseObject *dbObj;
 
+-(void) reloadMaptype;
 @end
--- Loc-Final/SecondViewController.h
+++ Loc-Final/SecondViewController.h
@@ -14,11 +14,21 @@
 @property (strong, nonatomic) IBOutlet UITableView *locationTable;
 
 @property (weak, nonatomic) IBOutlet UIDatePicker *searchdate;
+
 @property (strong, nonatomic) NSMutableArray *locationsArray;
 
 @property (strong, nonatomic) databaseObject *dbObj1;
 
+@property (weak, nonatomic) IBOutlet UIButton *btn_selectDate;
+
 @property (strong, nonatomic) NSMutableDictionary *locationToPush;
+
+@property (weak, nonatomic) IBOutlet UITextField *txt_selectedDate;
+
+-(void) searchDate:(NSDate*)date;
+- (IBAction)selectDate:(id)sender;
+- (IBAction)chagedValue:(id)sender;
+
 
 @end
 
